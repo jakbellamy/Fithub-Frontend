5cb8b5dd63a52b40232cd7278b340f623cefd09a
Jakob Bellamy <jakbellamy@gmail.com>
2019-10-02 03:51:46 -0500

    added tab icon. changed const for server route

--- src/constants.js
+++ src/constants.js
@@ -1,9 +1,9 @@
 export function API_AT(endpoint){
-    return `http://localhost:5000/${endpoint}`
+    return `http://localhost:5001/${endpoint}`
 }
 
 export function PRODUCTION_AT(endpoint){
-    return `http://159.89.177.40:5000/${endpoint}`
+    return `http://159.89.177.40:5001/${endpoint}`
 }
 
 export const loginErrorMsg = 'bad username or password'
